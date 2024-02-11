40f1c513a9d7e33edafa855fe4b8d92af30d10fa
yesdopepe <laiskashkash33@gmail.com>
2024-02-11 16:57:49 +0300

    testing if its gonna show on my commit history and reconfigured the directories

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import App from '../App';
+import App from './App';
 
 export default function mainscreen() {
   return (
